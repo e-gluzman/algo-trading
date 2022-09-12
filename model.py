cfadfcf5de4efe1e4c1501238a5109ce58b893a1
Eugene <egluzman@gmcs.ru>
2022-09-12 10:24:40 +0300

    added model

---
+++ model.py
@@ -0,0 +1,29 @@
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import GradientBoostingRegressor
+from sklearn.metrics import r2_score
+import pandas as pd
+
+def fit_predict(news_features, ticker, model='tree'):
+
+    X = pd.merge(news_features, ticker, how='inner', left_index= True, right_index=True)
+    y = X.Close.values
+    X.drop(columns=['Close'],inplace=True)  
+    
+    X_train, X_test, y_train, y_test = train_test_split(X,y,random_state=1)
+
+    if model == 'tree':
+        model = DecisionTreeRegressor()
+        model.fit(X_train,y_train)
+        ypred = model.predict(X_test)
+        r2 = r2_score(y_test,ypred)
+        importances = pd.DataFrame(model.feature_importances_,index=X.columns).sort_values(by=0,ascending=False)
+
+    if model == 'boosting':
+        model = GradientBoostingRegressor()
+        model.fit(X_train,y_train)
+        ypred = model.predict(X_test)
+        r2 = r2_score(y_test,ypred)
+        importances = pd.DataFrame(model.feature_importances_,index=X.columns).sort_values(by=0,ascending=False)
+
+    return model, r2, importances
--- run.py
+++ run.py
@@ -1,19 +1,35 @@
 import pandas as pd
 from nlp_extractor import extract_language_features
-
+from model import fit_predict
+import yfinance as yf
 
 # load news data acquired from wsj_scraper.py
 news = pd.read_csv('data/wsj_2018_2020.csv')
 
 # we don't need all the columns, lets keep only some of them
-
 keep = ['Business','U.S.','Letters','World','Commentary','Heard on the Street',
 'Politics','Review & Outlook','Finance','Tech','Europe','Markets','Earnings','Asia',
 'China','U.S. Economy','CFO Journal']
 
 news = news[news.subject.isin(keep)]
 
-# extract features that can be used in prediction from news text
+# extract features that can be used for predicting stock prices from news text
 features = extract_language_features(news, select_top_features = 100, method = 'count')
 
 print(features)
+
+# lets choose a few targets for prediction.
+OIL = yf.Ticker("CL=F").history(period='5y')
+DJI = yf.Ticker("^DJI").history(period='5y')
+
+model, r2, importances = fit_predict(features,OIL.Close)
+
+print('Oil result')
+print('r2_score: ' + str(r2))
+print(importances.head(10))
+
+model, r2, importances = fit_predict(features,DJI.Close)
+
+print('Dow Jones Industrial Average result')
+print('r2_score: ' + str(r2))
+print(importances.head(10))
